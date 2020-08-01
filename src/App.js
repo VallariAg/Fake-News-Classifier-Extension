7b967d9660e0e14786ada63f7149aa386e4994ea
VallariAg <val.agl002@gmail.com>
2020-08-01 12:15:51 +0530

    MVP of the extension
    Using
    - Hard coded input URL
    - Locally hosted server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './logo.png';
 import './App.css';
+import { Button, Paper } from '@material-ui/core';
 
 function App() {
+  const [result, setResult] = useState("")
+
+  // let result = <h3>
+  async function predict() {
+    const API_URL = "http://127.0.0.1:5000/predict";
+    console.log(API_URL)
+    await fetch(API_URL, {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'text/plain;charset=UTF-8'
+      }),
+      body: "https://stackoverflow.com/questions/25860304/how-do-i-set-response-headers-in-flask",
+    }).then(function (response) {
+      if (response.status !== 200) {
+        console.log(`Looks like there was a problem. Status code: ${response.status}`);
+        return;
+      }
+      response.json().then(function (data) {
+        console.log(data);
+        setResult(data['result'].toUpperCase());
+        console.log(data['result']);
+      });
+    })
+      .catch(function (error) {
+        console.log("Fetch error: " + error);
+      });
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button id="predict" color="primary" onClick={predict}>Predict news</Button>
+        <Paper elevation={3}> {result} </Paper>
       </header>
     </div>
   );
